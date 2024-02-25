44e5729d6b54c4976934362cd1085467c18d394d
Aidan Meehan <aodhan66@gmail.com>
2024-02-25 18:04:43 +0000

    pre routing changes

--- src/portfolio.tsx
+++ src/portfolio.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "preact/hooks";
-// import { Link } from "preact-router"
 // import preactLogo from './assets/preact.svg'
 // import viteLogo from '/vite.svg'
 import './app.css'
@@ -84,7 +83,7 @@ export function App() {
         title: "Habitica Homage",
         url: "https://habitica-homage.netlify.app/",
         description:
-          "Copied with no JavaScript to practice pure CSS",
+          "Copied with no JS to practice CSS",
         imgPath: "",
         imgUrl:
           "",
@@ -93,7 +92,7 @@ export function App() {
         order: 3,
       },
       {
-        title: "Dashboard?",
+        title: "Placeholder2",
         url: "",
         description:
           "Placeholder description",
@@ -105,7 +104,7 @@ export function App() {
         order: 4,
       },
       {
-        title: "Some API app?",
+        title: "Placeholder3",
         url: "",
         description:
           "Placeholder description",
@@ -118,92 +117,13 @@ export function App() {
       },
     ];
 
-    const skills: TechProp[] = [
-      {
-        name: "JavaScript",
-        imgPath: "/tech/javascript-logo.svg", 
-        imgUrl: "https://www.svgrepo.com/show/303206/javascript-logo.svg",
-      },
-      {
-        name: "TypeScript",
-        imgPath: "/tech/typescript-icon.svg", 
-        imgUrl: "https://www.svgrepo.com/show/354478/typescript-icon.svg",
-      },
-      {
-        name: "React",
-        imgPath: "/tech/react-logo.svg", 
-        imgUrl: "https://www.svgrepo.com/show/452092/react.svg",
-      },
-      {
-        name: "Node",
-        imgPath: "/tech/node-js.svg", 
-        imgUrl: "https://www.svgrepo.com/show/452075/node-js.svg",
-      },
-      {
-        name: "HTML",
-        imgPath: "/tech/html-5.svg", 
-        imgUrl: "https://www.svgrepo.com/show/452228/html-5.svg",
-      },
-      {
-        name: "CSS",
-        imgPath: "/tech/css-3.svg", 
-        imgUrl: "https://www.svgrepo.com/show/452185/css-3.svg",
-      },
-      {
-        name: "(No)SQL",
-        imgPath: "/tech/sql.svg", 
-        imgUrl: "https://www.svgrepo.com/show/374093/sql.svg",
-      },
-      {
-        name: "PostgreSQL",
-        imgPath: "/tech/postgresql.svg", 
-        imgUrl: "https://www.svgrepo.com/show/354200/postgresql.svg",
-      },
-      {
-        name: "MongoDB",
-        imgPath: "/tech/mongodb.svg", 
-        imgUrl: "https://www.svgrepo.com/show/373845/mongo.svg",
-      },
-      {
-        name: "Tailwind",
-        imgPath: "/tech/tailwind.svg", 
-        imgUrl: "https://www.svgrepo.com/show/374118/tailwind.svg",
-      },
-      {
-        name: "Material UI",
-        imgPath: "/tech/material-ui.svg", 
-        imgUrl: "https://www.svgrepo.com/show/354048/material-ui.svg",
-      },
-      {
-        name: "Electron",
-        imgPath: "/tech/electron.svg", 
-        imgUrl: "https://www.svgrepo.com/show/378796/electron.svg",
-      },
-    ]
-
   return (
     <div className="flex flex-col">
     <Header />
-  <div className="flex flex-col mb-6">
-     <div className="background self-center mb-6"> <h1 className="hero">Hi I'm Aidan.</h1></div>
-      <div className="mb-3 italic">
-        <p>I'm a Full-Stack Web Developer, currently in the UK.</p>
-        <p>I love working in close knit teams, and making useful, intuitive sites for the end user.</p>
-      </div>
-      <p>I also have an <strong><s>undiagnosed obsession</s></strong> keen interest in 
-      efficiency through all aspects of my life. As such, in the Blog section, you can find 
-      logs of my journey paring my multiday camping kit down to to ludicrous levels, as well as 
-      delusional diatribes about building ergonomic keyboards/input devices.</p>
-  </div>
 
   <div className="mb-6 flex flex-col align-items-center">
     <h2 className="text-xl font-black mb-2"><a href={`/portfolio`} className="justify-self-end link ">Portfolio</a></h2>
     <CardSection projects={projects} />
-  </div>
-
-  <div className="mb-6">
-    <h2 className="text-xl font-black mb-2">Skills</h2>
-    <TechSection skills={skills} />
   </div>
 
       <Footer isMobile={isMobile} />
